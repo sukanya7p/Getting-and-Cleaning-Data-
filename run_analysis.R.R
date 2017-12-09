7e1b331a7b762215ce49b15ca31d54f20e0c054f
sukanya7p <31965525+sukanya7p@users.noreply.github.com>
2017-12-09 13:22:04 -0500

    Update run_analysis.R.R
    This is the corrected version

--- run_analysis.R.R
+++ run_analysis.R.R
@@ -1,64 +1,44 @@
-# Below is the code to download and unzip the dataset
-
-if (!file.exists("./data")){dir.create("./data")}
+if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileUrl, destfile = "./data/dataset.zip")
-# Unzipping the dataset and saving the files in the data dorectory
-  unzip(zipfile = "./data/dataset.zip", exdir="./data")
-# Listing the data files in the data directory
-  list.files("./data")
- 
- 
-#Merging the "training" and the "test" data sets to create combined data set.
- 
-# Reading training data 
- x_train <- read.table("./data/UCI HAR Dataset/train/x_train.txt")
- y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
- subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
-# Reading testing data
- x_test<- read.table("./data/UCI HAR Dataset/test/x_test.txt")
- y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
- subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
-# Reading features vector
-features <- read.table("./data/UCI HAR Dataset/features.txt")
-# Reading activity label
-activityLabels <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
-# Assigning column names
-colnames(x_train) <- features[,2]
-colnames(y_train) <- "activityId"
-colnames(subject_train) <- "subjectId"
+download.file(fileUrl,destfile="./data/Dataset.zip")
 
-colnames(x_test) <- features[,2]
-colnames(y_test) <- "activityId"
-colnames(subject_test) <- "subjectId"
-colnames(activityLabels) <- c("activityId", "activityType")
-# Merging all datas into one large set
-mrg_train <- cbind(y_train, subject_train, x_train)
-mrg_test <- cbind(y_test, subject_test, x_test)
-setInAll <- rbind(mrg_train,mrg_test)
+# Unzip dataSet to /data directory
+unzip(zipfile="./data/Dataset.zip",exdir="./data")
+# Reading trainings tables:
+x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
 
-# Extracting only the measurements on the mean and standard deviation for each measurements
-# Reading column names
-colNames <- colnames(setInAll)
-# Creating a vector defining ID, Mean and Standard deviation
-mean_and_stdv <- (grepl("activityId", colNames)|
-                    grepl("subjectId", colNames)|
-                    grepl("mean.." , colNames)|
-                    grepl("std.." , colNames)
-                  )
-# Subsetting Means and Standard deviation
-subsetMean_Stdev <- setInAll[, mean_and_stdv==TRUE]
-# Using descriptive activity names to name the actiivity in the data set
-setWithActivityNames <- merge(subsetMean_Stdev, activityLabels,
-                              by='activityId',
-                              all.x=TRUE)
+# Reading testing tables:
+x_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
 
-# Creating a second independeent tidy data set with the average of each variable for each activity and each subject
-# Making second tidy data set
-secTidySet <- aggregate(.~subjectId+activityId, setWithActivityNames,mean)
+#read data description
+variable_names <- read.table("./data/UCI HAR Dataset/features.txt")
 
-secTidySet <- secTidySet[order(secTidySet$subjectId, secTidySet$activityId),]
+# read activity labels
+activity_labels <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
 
-# Writing second tidy data in text file
+# 1. Merges the training and the test sets to create one data set.
+X_total <- rbind(x_train, x_test)
+Y_total <- rbind(y_train, y_test)
+Subject_total <- rbind(subject_train, subject_test)
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+selected_var <- variable_names[grep("mean\\(\\)|std\\(\\)",variable_names[,2]),]
+X_total <- X_total[,selected_var[,1]]
 
+# 3. Uses descriptive activity names to name the activities in the data set
+colnames(Y_total) <- "activity"
+Y_total$activitylabel <- factor(Y_total$activity, labels = as.character(activity_labels[,2]))
+activitylabel <- Y_total[,-1]
+cd
+# 4. Appropriately labels the data set with descriptive variable names.
+colnames(X_total) <- variable_names[selected_var[,1],2]
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average
+# of each variable for each activity and each subject.
+colnames(Subject_total) <- "subject"
+total <- cbind(X_total, activitylabel, Subject_total)
+total_mean <- total %>% group_by(activitylabel, subject) %>% summarize_each(funs(mean))
+write.table(total_mean, file = "./data/UCI HAR Dataset/tidydata.txt", row.names = FALSE, col.names = TRUE)
 
